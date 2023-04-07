21d739fffd6f501e7a4f244b71ab204d7516334b
James McKeag <mckeag.james@yahoo.com>
2023-04-06 21:02:29 -0600

    commit tree 539285810405bc4bf613b52ce0ef807ab3518c9a

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,9 @@
 import React from "react";
 
-function Search() {
+function Search({ setSearch }) {
+  let handleChange = (e) => {
+    setSearch(e.target.value);
+  };
   return (
     <div className="searchbar">
       <label htmlFor="search">Search Plants:</label>
@@ -8,10 +11,11 @@ function Search() {
         type="text"
         id="search"
         placeholder="Type a name to search..."
-        onChange={(e) => console.log("Searching...")}
+        onChange={handleChange}
       />
     </div>
   );
 }
 
 export default Search;
+
